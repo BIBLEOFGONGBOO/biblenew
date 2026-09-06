26c47349d64684b3dd038e5559d22aeb43ca5321
BIBLEOFGONGBOO <budongsanedu@gmail.com>
2026-09-06 15:39:27 -0700

    Remove BIBLE_NEW_CONFIG definition
    Removed BIBLE_NEW_CONFIG object from config.js.

--- config.js
+++ config.js
@@ -1,6 +1,3 @@
-window.BIBLE_NEW_CONFIG = Object.freeze({
   supabaseUrl: 'https://yxudhflyxuztvzaiunva.supabase.co',
   publishableKey: '여기에 현재 biblenew에서 사용할 publishable key',
-  table: 'bible_question_translations',
-  build: '20260906-01'
-});
+
