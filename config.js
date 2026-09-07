3ce6381d80d268f27799b6207355417fcdfd1664
BIBLEOFGONGBOO <budongsanedu@gmail.com>
2026-09-06 18:09:31 -0700

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ window.BIBLE_NEW_CONFIG = Object.freeze({
   supabaseUrl: 'https://yxudhflyxuztvzaiunva.supabase.co',
   publishableKey: 'sb_publishable_9Kg6bvsSqZzOGMavBG3_1w_WO6WGbGB',
   table: 'bible_question_translations',
-  build: '20260906-12    '
+  build: '20260906-13    '
 });
