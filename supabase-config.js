84d9f08cd2a9f20decf2aafbef52a0ac7a56d719
BIBLEOFGONGBOO <budongsanedu@gmail.com>
2026-09-10 11:20:26 -0700

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,6 +0,0 @@
-window.BIBLE_SUPABASE_CONFIG = Object.freeze({
-  url: "https://yxudhflyxuztvzaiunva.supabase.co",
-  publishableKey: "sb_publishable_9Kg6bvsSqZzOGMavBG3_1w_WO6WGbGB",
-  questionFunction: 'bible-content',
-  enabled: true
-});
